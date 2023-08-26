5f6d61838d5c73a636339fb68c051a23f63074c9
suthineew <wsuthinee18@gmail.com>
2023-08-26 23:32:16 +0700

    create users list item component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from './UsersListItem';
 
 function UserList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
@@ -19,6 +20,7 @@ function UserList() {
   const handleUserAdd = () => {
     doCreateUser();
   };
+
   let content;
   if (isLoadingUsers) {
     content = <Skeleton times={6} className="h-10 w-full" />;
@@ -26,13 +28,7 @@ function UserList() {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
